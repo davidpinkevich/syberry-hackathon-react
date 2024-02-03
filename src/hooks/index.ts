539dc1145cc2bf90306350ac40fae2ea32f8bee8
Nikita Kolesnew <81689595+Jester175@users.noreply.github.com>
2024-02-03 12:03:04 +0300

    Revert "feature/service"

--- src/hooks/index.ts
+++
@@ -1,7 +0,0 @@
-import { useDispatch, useSelector, TypedUseSelectorHook } from "react-redux";
-import { RootState, AppDispatch } from "../redux/store";
-
-const useAppDispatch: () => AppDispatch = useDispatch;
-const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
-
-export { useAppDispatch, useAppSelector };
--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,9 @@
-import { BrowserRouter } from "react-router-dom";
+import React from "react";
 import ReactDOM from "react-dom/client";
-import { Provider } from "react-redux";
-import store from "./redux/store";
 import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <Provider store={store}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </Provider>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
