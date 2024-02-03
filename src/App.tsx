9ff99c82d30a359094da22ceeb5bec22a8494c4a
davidpinkevich <davidpinkevich95@gmail.com>
2024-02-03 12:12:58 +0300

    feat: add routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,23 @@
+import { Routes, Route } from "react-router";
+import Layout from "./pages/Layout/Layout";
+import PrivateRoute from "./hoc/PrivateRoute";
+
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<div>Main</div>} />
+          <Route
+            path="profile"
+            element={<PrivateRoute>Private</PrivateRoute>}
+          />
+        </Route>
+        <Route path="favorites" element={<div>Favorites</div>} />
+        <Route path=":id" element={<div>Film</div>} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
