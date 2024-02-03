80919782e22bfa18ee371cd54d2405fd52b3dbf2
davidpinkevich <davidpinkevich95@gmail.com>
2024-02-03 18:03:11 +0300

    fix: change profile

--- src/components/Header/Profile/Profile.tsx
+++ src/components/Header/Profile/Profile.tsx
@@ -1,38 +1,35 @@
-import { useState } from "react"
-import Favorites from "../../Favorites"
+import { useState } from "react";
+import Favorites from "../../Favorites";
 
-import styles from './styles.module.scss'
-import { SignIn } from "../../Forms/SignIn/SignIn"
-import { removeUser } from "../../../redux/slices/userSlice"
-import { useAppDispatch, useAppSelector } from '../../../hooks/index'
-import { Link } from "react-router-dom"
+import styles from "./styles.module.scss";
+import { SignIn } from "../../Forms/SignIn/SignIn";
+import { removeUser } from "../../../redux/slices/userSlice";
+import { useAppDispatch, useAppSelector } from "../../../hooks/index";
+import { Link } from "react-router-dom";
 
 const Profile = () => {
-    const dispatch = useAppDispatch();
-    const { user } = useAppSelector((state) => state.userSlice)
+  const dispatch = useAppDispatch();
+  const { user } = useAppSelector((state) => state.userSlice);
 
-    const [isOpen, setIsOpen] = useState(false)
-    const logout = () => {
-        dispatch(removeUser())
-    }
+  const [isOpen, setIsOpen] = useState(false);
+  const logout = () => {
+    dispatch(removeUser());
+  };
 
-    return (
-        <div className={styles.profile}>
-            {
-                !user
-                    ? <button onClick={() => setIsOpen(true)}>Войти</button>
-                    : <div>
-                        <Link to={'/profile'}>Личный кабинет</Link>
-                        <button onClick={logout}>Выйти</button>
-                    </div>
-            }
-            {
-                user && <Favorites />
-            }
-            {isOpen && <SignIn isOpen={isOpen} setIsOpen={setIsOpen} />}
-
+  return (
+    <div className={styles.profile}>
+      {!user ? (
+        <button onClick={() => setIsOpen(true)}>Войти</button>
+      ) : (
+        <div className={styles.block}>
+          <Link to={"/profile"}>Личный кабинет</Link>
+          <button onClick={logout}>Выйти</button>
         </div>
-    )
-}
+      )}
+      {user && <Favorites />}
+      {isOpen && <SignIn isOpen={isOpen} setIsOpen={setIsOpen} />}
+    </div>
+  );
+};
 
-export default Profile
+export default Profile;
