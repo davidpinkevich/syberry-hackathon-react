cd27221669554c22b5553df558bbec2f2005254b
davidpinkevich <davidpinkevich95@gmail.com>
2024-02-03 16:15:13 +0300

    feat: add fields

--- src/components/Film/AboutfFilm.tsx
+++ src/components/Film/AboutfFilm.tsx
@@ -1,51 +1,55 @@
-import React from 'react';
-import styles from './AboutfFilm.module.scss';
-import { Film } from '../../types';
+import styles from "./AboutfFilm.module.scss";
+import { Film } from "../../types";
+import ButtonPlay from "../ButtonPlay/ButtonPlay";
 
-const AboutfFilm = (props:Film) => {
-  const {genres, countries, filmLength, year} = props
+const AboutfFilm = (props: Film) => {
+  console.log("props: ", props);
+  const { genres, countries, duration, year, premiereRu } = props;
 
   return (
     <div className={styles.wrapper}>
-
-      <div className={styles.poster}></div>
+      <div className={styles.poster}>
+        <img src={props.posterUrlPreview} />
+      </div>
       <div className={styles.infoFilm}>
-
         <div className={styles.title}>
-          <h3>Догмен</h3>
-          <button className={styles.buttonTitle}>Смотреть</button>
+          <h3>{props.nameRu}</h3>
         </div>
-
         <div className={styles.detailFilm}>
-
-          <div className={styles.block1}>
-            <p className={styles.info}>Страна:</p>
-            <p className={styles.info}>Жанр:</p>
-            <p className={styles.info}>Продолжительность:</p>
-            <p className={styles.info}>Премьера &#040;Мир&#041;:</p>
-            <p className={styles.info}>Качество:</p>
-            <p className={styles.info}>Режиссер:</p>
+          <ButtonPlay />
+          <div className={styles.info}>
+            {<span>Страна:</span>}
+            <div className={styles.block}>
+              {countries?.map((item, i) => (
+                <div key={i}>
+                  <p>{item.country}</p>
+                </div>
+              ))}
+            </div>
           </div>
-          <div className={styles.block2}>
-          {countries?.map((item )=> (
-             <p> <span>{item.country}</span></p>)
-            )}
-            
-            {genres?.map((item )=> (
-             <p> <span>{item.genre}</span></p>)
-            )}
-            <p className={styles.info}> {filmLength}</p>
-            <p className={styles.info}> {year}</p>
-            <p className={styles.info}> качество</p>
-            <p className={styles.info}> режисер</p>
+          <div className={styles.info}>
+            {<span>Жанр:</span>}
+            <div className={styles.block}>
+              {genres?.map((item, i) => (
+                <div key={i}>
+                  <p>{item.genre} </p>
+                </div>
+              ))}
+            </div>
           </div>
-          
-
+          <p className={styles.info}>
+            {<span>Продолжительность:</span>} {duration}
+          </p>
+          <p className={styles.info}>
+            {<span>Год:</span>} {year} г.
+          </p>
+          <p className={styles.info}>
+            {<span>Премьера:</span>} {premiereRu}
+          </p>
         </div>
       </div>
-
     </div>
-  )
-}
+  );
+};
 
-export default AboutfFilm
+export default AboutfFilm;
