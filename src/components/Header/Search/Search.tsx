5ed29d6e16994979e4688a93178c4413a3607d12
davidpinkevich <davidpinkevich95@gmail.com>
2024-02-03 18:02:59 +0300

    fix: change search

--- src/components/Header/Search/Search.tsx
+++ src/components/Header/Search/Search.tsx
@@ -1,38 +1,49 @@
-import { useEffect, useState } from "react"
-import { Link } from "react-router-dom"
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
-import baseApi from '../../../services/baseApi'
+import baseApi from "../../../services/baseApi";
 
-import styles from './styles.module.scss'
-import { Film } from "../../../types"
+import styles from "./styles.module.scss";
+import { Film } from "../../../types";
 
 const Search = () => {
-    const [search, setSearch] = useState('')
-    const [films, setFilms] = useState<Film[] | undefined>([])
+  const [search, setSearch] = useState("");
+  const [films, setFilms] = useState<Film[] | undefined>([]);
 
-    const searchFilms = async () => {
-        const searchFilms = await baseApi.getFilmKeyword(search)
-        setFilms(searchFilms?.films)
-    }
+  const searchFilms = async () => {
+    const searchFilms = await baseApi.getFilmKeyword(search);
+    setFilms(searchFilms?.films);
+  };
 
-    useEffect(() => {
-        searchFilms()
-    }, [search])
-    return (
-        <div className={styles.search}>
-            <input type="search" value={search} onChange={(e) => setSearch(e.target.value)} placeholder="Введите фильм" />
-            {
-                search && (<ul className={styles.list}>
-                    {
+  useEffect(() => {
+    searchFilms();
+  }, [search]);
+  return (
+    <div className={styles.search}>
+      <p>Поиск:</p>
+      <input
+        type="search"
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+        placeholder="Введите фильм"
+      />
+      {search && (
+        <ul className={styles.list}>
+          {films &&
+            films.length > 0 &&
+            films.map((film) => (
+              <Link
+                className={styles.link_film}
+                key={film.filmId}
+                to={`/film/${film.filmId}`}
+              >
+                {film.nameRu}
+              </Link>
+            ))}
+        </ul>
+      )}
+    </div>
+  );
+};
 
-                        films && films.length > 0 && films.map(film => (
-                            <Link key={film.filmId} to={`/film/${film.filmId}`}>{film.nameRu}</Link>
-                        ))
-                    }
-                </ul>)
-            }
-        </div>
-    )
-}
-
-export default Search
+export default Search;
