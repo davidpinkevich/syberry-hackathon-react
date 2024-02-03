7fccb9457f992450933294e5359e69103a8b29d8
davidpinkevich <davidpinkevich95@gmail.com>
2024-02-03 14:39:01 +0300

    feat: add types

--- src/pages/Layout/MainPage/MainPage.tsx
+++ src/pages/Layout/MainPage/MainPage.tsx
@@ -1,14 +1,16 @@
-import { fetchPhotos } from "../../../redux/slices/sliceMain";
+import { fetchRandom, fetchTopFilms } from "../../../redux/slices/sliceMain";
 import { useAppDispatch, useAppSelector } from "../../../hooks";
 import { useEffect } from "react";
 
 const MainPage = () => {
   const dispatch = useAppDispatch();
-  const { randomFilm } = useAppSelector((state) => state.sliceMain);
-  console.log(randomFilm);
+  const { randomFilm, topFilms } = useAppSelector((state) => state.sliceMain);
+  console.log("randomFilm: ", randomFilm);
+  console.log("topFilms: ", topFilms);
   useEffect(() => {
     (async function () {
-      dispatch(fetchPhotos());
+      dispatch(fetchRandom());
+      dispatch(fetchTopFilms());
     })();
   }, []);
   return <div>MainPage</div>;
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,42 +1,42 @@
 export type Film = {
-    filmId: number;
-    nameRu: string;
-    nameEn: string;
-    type: string;
-    year: string;
-    description: string;
-    filmLength: string;
-    countries: Country[];
-    genres: Genre[];
-    rating: string;
-    ratingVoteCount: number;
-    posterUrl: string;
-    posterUrlPreview: string;
-}
+  filmId: number;
+  nameRu: string;
+  nameEn: string;
+  type: string;
+  year: string;
+  description: string;
+  filmLength: string;
+  countries: Country[];
+  genres: Genre[];
+  rating: string;
+  ratingVoteCount: number;
+  posterUrl: string;
+  posterUrlPreview: string;
+};
 
 export type TopFilmsResponse = {
-    total: number;
-    totalPages: number;
-    items: Film[];
-}
+  total: number;
+  totalPages: number;
+  items: Film[];
+};
 
 export type SimilarFilmsResponse = {
-    total: number;
-    totalPages: number;
-    items: Film[];
-}
+  total: number;
+  totalPages: number;
+  items: Film[];
+};
 
 export type FilmSearchResponse = {
-    keyword: string;
-    pagesCount: number;
-    searchFilmsCountResult: number;
-    films: Film[];
-}
+  keyword: string;
+  pagesCount: number;
+  searchFilmsCountResult: number;
+  items: Film[];
+};
 
 export type Genre = {
-    genre: string;
-}
+  genre: string;
+};
 
 export type Country = {
-    country: string;
-}
+  country: string;
+};
