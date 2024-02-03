460800917605b587384986eaf3209bc2ed9b2c5b
Nikita Kolesnew <chattrain.kol7@gmail.com>
2024-02-03 15:05:14 +0300

    feat: created a swiper

---
+++ src/components/SwiperFilms/SwiperFilms.tsx
@@ -0,0 +1,31 @@
+import { FC } from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation } from "swiper/modules";
+
+import "swiper/css";
+import "swiper/css/navigation";
+import styles from './swiper.module.scss'
+
+export const SwiperFilms: FC = () => {
+  return (
+    <>
+      <Swiper
+        navigation={true}
+        modules={[Navigation]}
+        slidesPerView={4}
+        spaceBetween={30}
+        className={`${styles.swiper} mySwiper`}
+      >
+        <SwiperSlide>Slide 1</SwiperSlide>
+        <SwiperSlide>Slide 2</SwiperSlide>
+        <SwiperSlide>Slide 3</SwiperSlide>
+        <SwiperSlide>Slide 4</SwiperSlide>
+        <SwiperSlide>Slide 5</SwiperSlide>
+        <SwiperSlide>Slide 6</SwiperSlide>
+        <SwiperSlide>Slide 7</SwiperSlide>
+        <SwiperSlide>Slide 8</SwiperSlide>
+        <SwiperSlide>Slide 9</SwiperSlide>
+      </Swiper>
+    </>
+  );
+};
